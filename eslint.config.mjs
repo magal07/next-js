e63adbcda1650c82c7d2c8cb1aa2b8ab7e16ae39
Marcelo Magalhães <celoxd01@gmail.com>
2025-05-27 20:57:19 -0300

    Next-JS - Base

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,38 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import js from '@eslint/js';
+import prettierPlugin from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
+import tseslint from 'typescript-eslint';
+import reactPlugin from 'eslint-plugin-react';
+import { defineConfig } from 'eslint/config';
+import globals from 'globals';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
-
-export default eslintConfig;
+export default defineConfig([
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    plugins: {
+      prettier: prettierPlugin,
+      react: reactPlugin,
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      'react/react-in-jsx-scope': 'off', // Next.js não exige React importado
+    },
+  },
+  // Isso aplica as regras do Prettier como config final (desabilita conflitos do ESLint)
+  prettierConfig,
+  js.configs.recommended,
+  tseslint.configs.recommended,
+]);
